18bcd3d5240fc9dd1bd82265c662ffd4cd4ec6c4
Paula Carril Torrens <PAULAANDREACARRIL@GMAIL.COM>
2021-10-28 19:10:26 +1100

    API call, useState weatherData,

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Weather from "./Weather.js";
 function App() {
   return (
     <div className="App">
-      <Weather city="Armidale" />
+      <Weather />
+      <p className="sign">
+        Coded by{" "}
+        <a href="https://github.com/paulacarrilt" alt="mygit">
+          Paula Carril Torrens
+        </a>
+      </p>
     </div>
   );
 }
